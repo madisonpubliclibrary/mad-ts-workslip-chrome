58b8e350da181a7c68098ae55cc4dbc6940a81c1
Lucas Schneider <lrschneider94@gmail.com>
2019-04-04 16:51:44 -0500

    Progress

--- background.js
+++ background.js
@@ -1,67 +1,99 @@
 chrome.contextMenus.create({
   "id": "print-workslip",
   "title": "Print MAD-TS Workslip",
+  "documentUrlPatterns": [
+    "https://scls-mad-staff.kohalibrary.com/cgi-bin/koha/acqui/getit.pl",
+    "https://scls-mad-staff.kohalibrary.com/getit/public/index.html"],
   "contexts": ["all"],
   "visible": true
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "print-workslip") {
-    chrome.tabs.executeScript(tab.id, {
-      "file": "scrapeGetIt.js"
-    }, (resultArr) => {
-      if (resultArr.length > 0) {
-        let data = resultArr[0];
+    printWorkslip(tab);
+  }
+});
 
-        let getHolds = new Promise(function(resolve, reject) {
-          if (data.bibRecId.length > 0) {
-            chrome.tabs.create({
-              "url": "https://scls-staff.kohalibrary.com/cgi-bin/koha/catalogue/detail.pl?biblionumber=" + data.bibRecId,
-              "active": false
-            }, function(holdsTab) {
-              chrome.tabs.executeScript(holdsTab.id, {
-                "file": "getNumHolds.js"
-              }, holdsArr => {
-                chrome.tabs.remove(holdsTab.id);
-                resolve(holdsArr[0]);
-              });
-            });
-          } else {
-            resolve('');
-          }
-        });
-
-        let getMARCData = new Promise(function(resolve, reject) {
-          if (data.bibRecId.length > 0) {
-            chrome.tabs.create({
-              "url": "https://scls-staff.kohalibrary.com/cgi-bin/koha/cataloguing/addbiblio.pl?biblionumber=" + data.bibRecId,
-              "active": false
-            }, function(marcTab) {
-              chrome.tabs.executeScript(marcTab.id, {
-                "file": "getMARCData.js"
-              }, marcArr => {
-                chrome.tabs.remove(marcTab.id);
-                resolve(marcArr[0]);
-              });
-            });
-          } else {
-            resolve('');
-          }
-        });
-
-        Promise.all([getHolds, getMARCData]).then(res => {
-          data.holds = res[0];
-          data.marcData = res[1];
-
-          chrome.tabs.create({
-            "url": chrome.runtime.getURL("workslip.html")
-          }, function(tab) {
-            setTimeout(function() {
-              chrome.tabs.sendMessage(tab.id, data);
-            }, 100);
-          });
-        });
-      }
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.key === 'printWorkslip') {
+    chrome.tabs.query({'currentWindow': true, 'active': true}, tabs => {
+      printWorkslip(tabs[0]);
     });
   }
 });
+
+function printWorkslip(tab) {
+  chrome.tabs.executeScript(tab.id, {
+    "file": "scrapeGetIt.js"
+  }, (resultArr) => {
+    if (resultArr.length > 0) {
+      let data = resultArr[0];
+
+      let getHolds = new Promise(function(resolve, reject) {
+        if (data.bibRecId.length > 0) {
+          chrome.tabs.create({
+            "url": "https://scls-staff.kohalibrary.com/cgi-bin/koha/catalogue/detail.pl?biblionumber=" + data.bibRecId,
+            "active": false
+          }, function(holdsTab) {
+            chrome.tabs.executeScript(holdsTab.id, {
+              "file": "getNumHolds.js"
+            }, holdsArr => {
+              chrome.tabs.remove(holdsTab.id);
+
+              if (holdsArr[0] === 'holdsError') {
+                reject('Unable to find item holds data; not logged into Koha.');
+              } else {
+                resolve(holdsArr[0]);
+              }
+            });
+          });
+        } else {
+          resolve('');
+        }
+      });
+
+      let getMARCData = new Promise(function(resolve, reject) {
+        if (data.bibRecId.length > 0) {
+          chrome.tabs.create({
+            "url": "https://scls-staff.kohalibrary.com/cgi-bin/koha/cataloguing/addbiblio.pl?biblionumber=" + data.bibRecId,
+            "active": false
+          }, function(marcTab) {
+            chrome.tabs.executeScript(marcTab.id, {
+              "file": "getMARCData.js"
+            }, marcArr => {
+              chrome.tabs.remove(marcTab.id);
+
+              if (marcArr[0] === 'marcError') {
+                reject('Unable to find MARC data; not logged into Koha.');
+              } else {
+                resolve(marcArr[0]);
+              }
+            });
+          });
+        } else {
+          resolve('');
+        }
+      });
+
+      Promise.all([getHolds, getMARCData]).then(res => {
+        data.holds = res[0];
+        data.marcData = res[1];
+
+        chrome.tabs.create({
+          "url": chrome.runtime.getURL("workslip.html")
+        }, function(tab) {
+          setTimeout(function() {
+            chrome.tabs.sendMessage(tab.id, data, () => {
+              chrome.tabs.remove(tab.id);
+            });
+          }, 250);
+        });
+      }, reject => {
+        chrome.tabs.create({
+          "url": "https://scls-staff.kohalibrary.com",
+          "active": true
+        });
+      });
+    }
+  });
+}
--- scrapeGetIt.js
+++ scrapeGetIt.js
@@ -6,39 +6,39 @@
   if (contentDoc) {
     contentDoc = contentDoc.contentWindow.document;
 
-    let title = contentDoc.querySelector('input[ng-model="formdata.title"]');
+    let title = contentDoc.querySelector('.active input[ng-model="formdata.title"]');
     data.title = title ? title.value : '';
-    let author = contentDoc.querySelector('input[ng-model="formdata.author"]');
+    let author = contentDoc.querySelector('.active input[ng-model="formdata.author"]');
     data.author = author ? author.value : '';
-    let callNumber = contentDoc.querySelector('input[ng-model="formdata.EAN13"]');
+    let callNumber = contentDoc.querySelector('.active input[ng-model="formdata.EAN13"]');
     data.callNumber = callNumber ? callNumber.value : '';
-    let isbn = contentDoc.querySelector('input[ng-model="formdata.ISBN"]');
+    let isbn = contentDoc.querySelector('.active input[ng-model="formdata.ISBN"]');
     data.isbn = isbn ? isbn.value : '';
-    let issn = contentDoc.querySelector('input[ng-model="formdata.ISSN"]');
+    let issn = contentDoc.querySelector('.active input[ng-model="formdata.ISSN"]');
     data.issn = issn ? issn.value : '';
-    let ismn = contentDoc.querySelector('input[ng-model="formdata.ISMN"]');
+    let ismn = contentDoc.querySelector('.active input[ng-model="formdata.ISMN"]');
     data.ismn = ismn ? ismn.value : '';
-    let upc = contentDoc.querySelector('input[ng-model="formdata.UPC"]');
+    let upc = contentDoc.querySelector('.active input[ng-model="formdata.UPC"]');
     data.upc = upc ? upc.value : '';
-    let manufactNum = contentDoc.querySelector('input[ng-model="formdata.manufacturer_number"]');
+    let manufactNum = contentDoc.querySelector('.active input[ng-model="formdata.manufacturer_number"]');
     data.manufactNum = manufactNum ? manufactNum.value : '';
-    let supplierNum = contentDoc.querySelector('input[ng-model="formdata.supplier_number"]');
+    let supplierNum = contentDoc.querySelector('.active input[ng-model="formdata.supplier_number"]');
     data.supplierNum = supplierNum ? supplierNum.value : '';
-    let publisher = contentDoc.querySelector('input[ng-model="formdata.publisher"]');
+    let publisher = contentDoc.querySelector('.active input[ng-model="formdata.publisher"]');
     data.publisher = publisher ? publisher.value : '';
-    let listPrice = contentDoc.querySelector('input[ng-model="formdata.list_price"]');
+    let listPrice = contentDoc.querySelector('.active input[ng-model="formdata.list_price"]');
     data.listPrice = listPrice ? listPrice.value : '';
-    let discountedPrice = contentDoc.querySelector('input[ng-model="formdata.discounted_price"]');
+    let discountedPrice = contentDoc.querySelector('.active input[ng-model="formdata.discounted_price"]');
     data.discountedPrice = discountedPrice ? discountedPrice.value : '';
-    let datePub = contentDoc.querySelector('input[ng-model="formdata.date_of_publication"]');
+    let datePub = contentDoc.querySelector('.active input[ng-model="formdata.date_of_publication"]');
     data.datePub = datePub ? datePub.value : '';
-    let edition = contentDoc.querySelector('input[ng-model="formdata.edition"]');
+    let edition = contentDoc.querySelector('.active input[ng-model="formdata.edition"]');
     data.edition = edition ? edition.value : '';
-    let description = contentDoc.querySelector('input[ng-model="formdata.description"]');
+    let description = contentDoc.querySelector('.active input[ng-model="formdata.description"]');
     data.description = description ? description.value : '';
-    let bibRecId = contentDoc.querySelector('input[ng-model="formdata.bibliographic_record_id"]');
+    let bibRecId = contentDoc.querySelector('.active input[ng-model="formdata.bibliographic_record_id"]');
     data.bibRecId = bibRecId ? bibRecId.value : '';
-    let orderLineRef = contentDoc.querySelector('input[ng-model="formdata.order_line_reference"]');
+    let orderLineRef = contentDoc.querySelector('.active input[ng-model="formdata.order_line_reference"]');
     if (orderLineRef && orderLineRef.value.length > 0) {
       data.orderLineRef = orderLineRef.value;
       let orderLineRefParts = orderLineRef.value.split('-');
@@ -50,7 +50,7 @@
     }
 
     data.copies = [];
-    let copyTable = contentDoc.querySelector('div[ng-include="\'app/purchase_order_line_copies/index.html\'"] .ngCanvas');
+    let copyTable = contentDoc.querySelector('.active div[ng-include="\'app/purchase_order_line_copies/index.html\'"] .ngCanvas');
 
     if (copyTable) {
       for (let row of copyTable.children) {
